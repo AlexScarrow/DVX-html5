51ab648f35b880e8668fbb69532c4bc622b4f05a
AlexScarrow <alex.scarrow@gmail.com>
2026-02-16 15:04:09 +0000

    Add multi-unit squad controls, AP-driven movement, and left-side tactical HUD scaffolding

---
+++ main/ui_panel.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"derple_ui_panel\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/ui_panel.atlas\"\n"
+  "}\n"
+  ""
+}

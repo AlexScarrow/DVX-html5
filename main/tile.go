742607fb827dedbabb0286c84b9a2816c77f6031
AlexScarrow <alex.scarrow@gmail.com>
2026-02-17 13:57:24 +0000

    Add alien prototype loop and semi-transparent tile rendering

---
+++ main/tile.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"tile_corridor\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/tiles.atlas\"\n"
+  "}\n"
+  ""
+}

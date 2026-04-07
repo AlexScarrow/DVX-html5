3f384fe53b092806d1cd3dad979e1e10e8d95d4d
AlexScarrow <alex.scarrow@gmail.com>
2026-04-07 20:25:33 +0100

    Split outro rendering into a dedicated atlas and marker pipeline.

---
+++ main/outro_marker.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"outro_purge_win_spaceship_fadeBlack\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/outro.atlas\"\n"
+  "}\n"
+  ""
+}
